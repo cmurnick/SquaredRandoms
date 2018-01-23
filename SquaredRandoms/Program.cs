c1f3ac8f59ebf795577ed50fc713dc55d91b3520
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-01-22 18:16:31 -0600

    works

--- SquaredRandoms/Program.cs
+++ SquaredRandoms/Program.cs
@@ -10,6 +10,43 @@ namespace SquaredRandoms
     {
         static void Main(string[] args)
         {
+
+            var list = RandomNumberList();
+            var squaredList = new List<int>();
+
+            foreach ( var number in list)
+            {
+                
+                var squaredNumber = number * number;
+                
+                var even = squaredNumber % 2 == 0;
+                if(even) 
+                {
+                    //if it's odd, I don't want it to print to the screen
+                    squaredList.Add(squaredNumber);
+                    Console.WriteLine(squaredNumber);
+                }
+                
+            }
+             Console.ReadLine();
+        }
+
+        private static List<int> RandomNumberList()
+        {
+            var count = 20;
+            Random rnd = new Random();
+            List<int> result = new List<int>();
+            //var strings = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+
+                var randomNumber = rnd.Next(1, 51);
+                result.Add(randomNumber);
+
+            }
+
+            return result;
         }
     }
 }
